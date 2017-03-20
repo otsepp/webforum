c71f1d099d4f80ab51c78560d3ad2e6230f952c6
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-20 18:39:50 +0200

    adminoikeus säätöä

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320123836) do
+ActiveRecord::Schema.define(version: 20170320155018) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -35,7 +35,8 @@ ActiveRecord::Schema.define(version: 20170320123836) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string  "username"
+    t.boolean "admin",    default: false
   end
 
 end
