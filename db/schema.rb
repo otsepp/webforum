e0ccdaa3dffa703101ebc59f26e93a193399aada
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-20 10:38:20 +0200

    lisätty user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320074128) do
+ActiveRecord::Schema.define(version: 20170320082621) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170320074128) do
     t.datetime "updated_at", null: false
     t.integer  "subject_id"
     t.string   "content"
+    t.integer  "user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -31,6 +32,10 @@ ActiveRecord::Schema.define(version: 20170320074128) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
