77b9674f5d3e2f10da9d880d37261dd1c3269c58
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-20 18:49:38 +0200

    placeholders

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,7 @@ class Subject < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	belongs_to :category
 	has_many :messages, dependent: :destroy
+
+	#lisää col creator_id
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
 	has_many :messages, dependent: :destroy
+	
+	#has_many :subjects
+
 	validates :username, presence: true, uniqueness: true
+
+	#placeholder
+	def has_modify_rights_to_subject(subject)
+		return true
+	end
+
+	#placeholder
+	def has_modify_rights_to_message(message)
+		return true
+	end
+
 end
