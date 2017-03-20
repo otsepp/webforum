695853a9d8cabca05311e58176d05783c7a41234
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-20 14:51:58 +0200

    alkeinen kirjautuminen

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   resources :messages
   resources :subjects
   resources :categories
+  resource :session, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'categories#index'
+
+get 'signup', to: 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320082621) do
+ActiveRecord::Schema.define(version: 20170320123836) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170320082621) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
   end
 
 end
