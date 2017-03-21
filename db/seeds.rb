4a3c1188c7fd92693054f9b3e8b1c59f20275af9
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-21 19:56:05 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,18 @@ Category.create!([
   {name: "Music"}
 ])
 Message.create!([
-  {subject_id: 1, content: "How do I into PC gaming??", user_id: 1},
-  {subject_id: 1, content: "purchase an alienware", user_id: 2}
+  {subject_id: 18, content: "Wat do u think", user_id: 2},
+  {subject_id: 18, content: "1st warning\r\n", user_id: 7},
+  {subject_id: 20, content: "What is the best sports car", user_id: 2},
+  {subject_id: 21, content: "DONT DO IT OR BAN", user_id: 7},
+  {subject_id: 21, content: "o rly?!", user_id: 2},
+  {subject_id: 38, content: "wat are the best\r\n", user_id: 7}
 ])
 Subject.create!([
-  {name: "PC gaming", category_id: 1, user_id: 1}
+  {name: "Nintendo sux", category_id: 1, user_id: 7},
+  {name: "sport cars", category_id: 2, user_id: 2},
+  {name: "PSA: no flame wars", category_id: 1, user_id: 7},
+  {name: "Nes games", category_id: 1, user_id: 7}
 ])
 User.create!([
   {username: "magikian", admin: false},
