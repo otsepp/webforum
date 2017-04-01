f8c1708866a8d1503e32a7bafee4ba03149d8eb9
otsepp <o.tuomas.seppanen@gmail.com>
2017-04-01 11:54:45 +0300

    seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,15 @@ Message.create!([
   {subject_id: 20, content: "What is the best sports car", user_id: 2},
   {subject_id: 21, content: "DONT DO IT OR BAN", user_id: 7},
   {subject_id: 21, content: "o rly?!", user_id: 2},
-  {subject_id: 38, content: "wat are the best\r\n", user_id: 7}
+  {subject_id: 38, content: "wat are the best\r\n", user_id: 7},
+  {subject_id: 39, content: "no bully plz!", user_id: 7}
 ])
 Subject.create!([
   {name: "Nintendo sux", category_id: 1, user_id: 7},
   {name: "sport cars", category_id: 2, user_id: 2},
   {name: "PSA: no flame wars", category_id: 1, user_id: 7},
-  {name: "Nes games", category_id: 1, user_id: 7}
+  {name: "Nes games", category_id: 1, user_id: 7},
+  {name: "PSA: no bullying", category_id: 1, user_id: 7}
 ])
 User.create!([
   {username: "magikian", admin: false},
