70dc641ab1136cdf1a717c5a9c73000b0629fb53
otsepp <o.tuomas.seppanen@gmail.com>
2017-04-25 11:43:08 +0300

    colors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( messages.css )
+
+Rails.application.config.assets.precompile += %w( colors.css )
