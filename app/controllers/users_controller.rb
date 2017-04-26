a174eaf64398f298908e6947ddb79c9dfe84cb7f
otsepp <o.tuomas.seppanen@gmail.com>
2017-04-26 09:23:41 +0300

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,12 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+	@creating = true
   end
 
   # GET /users/1/edit
   def edit
+	@creating  = false
   end
 
   # POST /users
@@ -28,6 +30,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)	
     respond_to do |format|
       if @user.save
+	session[:user_id] = @user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
