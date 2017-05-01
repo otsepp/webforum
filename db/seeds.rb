69b21b3fab30bba67e695373e742d8eb0172c412
otsepp <o.tuomas.seppanen@gmail.com>
2017-05-01 14:55:38 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
+c1 = Category.create name: "Gaming"
+
 User.create username: "admin", admin: true, signature: "Respect my authority", password: "pass", password_confirmation: "pass"
 User.create username: "gamer_mod", signature: "World Peace", moderator_category_id: c1.id
